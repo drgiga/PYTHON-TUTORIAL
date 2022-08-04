306853adf7104c8801d7fd6e265434f5a5782386
drgiga <drgiga1@gmail.com>
2022-08-04 21:53:24 +0430

    modify in session01

--- session01/session01.py
+++ session01/session01.py
@@ -1,3 +1,3 @@
 # Printing a String with print() function
-print('Hello world')
-print('this is first python function to learn.') 
+print('Hello world') # Using with Single Quotation ('')
+print("this is first python function to learn.") # Using with Double Quotation ("")
