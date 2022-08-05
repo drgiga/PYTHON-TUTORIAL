5e6756355d5835e3dcd0f14a5050c3f9a1bc0ecc
drgiga <drgiga1@gmail.com>
2022-08-06 00:18:33 +0430

    Sets

---
+++ session09/session09_Set.py
@@ -0,0 +1,27 @@
+stringSet = {"apple", "banana", "cherry", "apple", "cherry"," "}
+print(stringSet)
+
+stringSet2 = {'apple', 'banana', 'cherry', 'apple', 'cherry',' '}
+print(stringSet2)
+
+integerSet = {1,200,89754520,999999999999999999999999999999999999999999999999999,0}
+floatSet = {1.0,20058.02154,897.54520,999999999999999999999999999999999999999999999999999.02020202,0.0}
+complexSet = {1j+8,-2j+74,7+3j,3+6j}
+booleanSet = {True,True,False,True,False,False}
+setOfAllTypes = {1.0,"Hello",145,-1j,3j+8,'world',False,True,None}
+# setOfSet = {{"name","ghghfg"}} # TypeError: unhashable type: 'set'
+# setOfList = {["name","ghghfg"]} # TypeError: unhashable type: 'list'
+# setOfDict = {{"name":"ghghfg"}} # TypeError: unhashable type: 'dict'
+
+print("------------------------------------")
+print(stringSet)
+print(stringSet2)
+print(integerSet)
+print(floatSet)
+print(complexSet)
+print(booleanSet)
+print("------------------------------------")
+print(setOfAllTypes)
+print("------------------------------------")
+
+
