f73c8cdbd79e916b72da57dd642cd7064ee478d1
drgiga <drgiga1@gmail.com>
2022-08-06 00:45:38 +0430

    Tuple

---
+++ session10/session10_tuples.py
@@ -0,0 +1,69 @@
+stringTuple = ("apple", "banana", "cherry", "apple", "cherry"," ")
+print(stringTuple)
+
+stringTuple2 = ('apple', 'banana', 'cherry', 'apple', 'cherry',' ')
+print(stringTuple2)
+
+integerTuple = (1,200,89754520,999999999999999999999999999999999999999999999999999,0)
+floatTuple = (1.0,20058.02154,897.54520,999999999999999999999999999999999999999999999999999.02020202,0.0)
+complexTuple = (1j+8,-2j+74,7+3j,3+6j)
+booleanTuple = (True,True,False,True,False,False)
+allTypesTuple = (1.0,"Hello",145,-1j,3j+8,'world',False,True,None)
+tupleOfIntegers = ({1,2,3},{4,5,6}) 
+tupleOfFloats = ({1.0,20.25,3854.3595},{4987.2500,5.0,606.999}) 
+tupleOfComplexes = ({1j+8,-2j+74,7+3j,3+6j},{-1j+8,2j+74,-7+3j,-3+6j}) 
+
+tupleOf1 = ([1,2,3],[4,5,6]) 
+tupleOf2 = ([1.0,20.25,3854.3595],[4987.2500,5.0,606.999]) 
+tupleOf3 = ([1j+8,-2j+74,7+3j,3+6j],[-1j+8,2j+74,-7+3j,-3+6j]) 
+tupleOf4 = (['apple', 'banana'],['cherry', 'apple', 'cherry',' ']) 
+tupleOf5 = (
+            ['apple', 'banana'],
+            {'cherry', 'apple', 'cherry',' '},
+            {"key1":1,
+            "key2":"ali",
+            "key3":None,
+            "key4":frozenset({1,2,3})},
+            None,
+            frozenset({1.0,20,3+4j}),
+            True,
+            False,
+            [(1,2,3)],
+            ([5.0,5.25]),
+            {},
+            {" ",' '}
+    ) 
+
+print("------------------------------------")
+print(stringTuple)
+print("------------------------------------")
+print(stringTuple2)
+print("------------------------------------")
+print(integerTuple)
+print("------------------------------------")
+print(floatTuple)
+print("------------------------------------")
+print(complexTuple)
+print("------------------------------------")
+print(booleanTuple)
+print("------------------------------------")
+print(allTypesTuple)
+print("------------------------------------")
+print(tupleOfIntegers)
+print("------------------------------------")
+print(tupleOfFloats)
+print("------------------------------------")
+print(tupleOfComplexes)
+print("------------------------------------")
+print(tupleOf1)
+print("------------------------------------")
+print(tupleOf2)
+print("------------------------------------")
+print(tupleOf3)
+print("------------------------------------")
+print(tupleOf4)
+print("------------------------------------")
+print(tupleOf5)
+print("------------------------------------")
+
+
