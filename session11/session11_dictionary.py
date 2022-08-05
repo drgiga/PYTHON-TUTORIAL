a5e12acb42d311593eb55572778676fb18b0a94e
drgiga <drgiga1@gmail.com>
2022-08-06 01:04:10 +0430

    Dictionary

---
+++ session11/session11_dictionary.py
@@ -0,0 +1,18 @@
+stringDictionary = {"name":"Davoud", "family":"Keramati", "City":"Tehran", "Country":"Iran", "empty_char":" "}
+print(stringDictionary)
+
+print("------------------------------------")
+var1 = "var1"
+var2 = "var2"
+var3 = "var3"
+stringDictionary2 = {"val1":var1,"val2":var2,"val3":var3}
+print(stringDictionary2)
+print("------------------------------------")
+dict1 = {1:1,2:2,3:5821,4:987.002541,5:None,6:{1,2,3},7:(1,2,3),8:frozenset({"1","2","3"}),9:"fgdg dfgdfgd dfgdfgd"}
+print(dict1)
+dict2 = {"1":1,"2":2,"3":5821,"4":987.002541,"5":None,"6":{1,2,3},"7":(1,2,3),"8":frozenset({"1","2","3"}),"9":"fgdg dfgdfgd dfgdfgd"}
+print(dict2)
+dict3 = {"1":1,"2":2,"3":5821,"4":987.002541,"5":None,"6":({1,2,3,True,None," "}),"7":{(1,2,3,None)},"8":frozenset({"1","2","3"}),"9":"fgdg dfgdfgd dfgdfgd","tag":'ایران',"10":[{"key":None}]}
+print(dict3)
+
+
