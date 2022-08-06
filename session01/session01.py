929c46c2689bf5cb158c40ccb0d9b109f865907f
drgiga <drgiga1@gmail.com>
2022-08-06 13:30:49 +0430

    print("Hello World")

--- session01/session01.py
+++ session01/session01.py
@@ -1,3 +1,4 @@
 # Printing a String with print() function
 print('Hello world') # Using with Single Quotation ('')
 print("this is first python function to learn.") # Using with Double Quotation ("")
+print('----------------')
